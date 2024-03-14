443e1f4ffeeeb98b1b7d75f6390eb2b8ccf3ba9e
DeaneAlexander <DeaneAustenAlexander@gmail.com>
2024-03-14 13:06:33 -0400

    Pixel Pets My near wallet backend push

--- Assets/Scripts/Blockchain/NearHelper.cs
+++ Assets/Scripts/Blockchain/NearHelper.cs
@@ -16,7 +16,7 @@ public enum DataGetState
 
 public class NearHelper : MonoBehaviour
 {
-    string WalletUri = "https://wallet.testnet.near.org";
+    string WalletUri = "https://testnet.mynearwallet.com";
     string ContractId = "pixeltoken.testnet";
     string WalletSuffix = ".testnet";
     string BackendUri = "https://dev-testnet.pixeldapps.co/api";
@@ -41,7 +41,7 @@ public class NearHelper : MonoBehaviour
         if (!Testnet)
         {
             BackendUri = "https://ecosystem.pixeldapps.co/api";
-            WalletUri = "https://wallet.near.org";
+            WalletUri = "https://www.mynearwallet.com";
             ContractId = "pixeltoken.near";
             WalletSuffix = ".near";
         }
